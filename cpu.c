908e14c4c89f1c1b91a555e31a8e2f27c35dc7ca
frynor <leolomtadze6002@gmail.com>
2024-11-27 19:42:31 +0400

    Added GPU display

--- cpu.c
+++ cpu.c
@@ -1,6 +1,42 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+void gpuInfo(char* buffer, size_t size) {
+	FILE *file = fopen("/proc/driver/nvidia/gpus/0000:01:00.0/information", "r");
+	
+	if (file == NULL) {
+		perror("Failed to open GPU file!\n");
+		return;
+	}
+	
+	while (fgets(buffer, size, file)) {
+		char* model_name = strstr(buffer, "Model:");
+		if (model_name != NULL) {
+			model_name += strlen("Model:");
+
+			while (isspace((unsigned char)*model_name)) {
+				model_name++;
+			}
+
+			char* model_name_end = strchr(model_name, '\n');
+			if (model_name_end != NULL) {
+				*model_name_end = '\0';
+			}
+
+			char* end = model_name + strlen(model_name) - 1;
+			while (end > model_name && *end == ' ') {
+				end--;
+			}
+			*(end + 1) = '\0';
+			printf("GPU: %s\n", model_name);
+			break;
+		}		
+	}
+
+	fclose(file);
+}
 
 void memInfo(char* buffer, size_t size) {
 	FILE *file = fopen("/proc/meminfo", "r");
@@ -140,6 +176,7 @@ int main() {
 	cpuInfo(buffer, size);
 	linuxInfo(buffer, size);
 	memInfo(buffer, size);
+	gpuInfo(buffer, size);
 
 	return 0;
 	
