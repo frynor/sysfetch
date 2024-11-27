d8b6b8c860a119fdaf24a2c312455f1e3502eba2
frynor <leolomtadze6002@gmail.com>
2024-11-27 18:37:15 +0400

    Added memory display

--- cpu.c
+++ cpu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void memInfo(char* buffer, size_t size) {
 	FILE *file = fopen("/proc/meminfo", "r");
@@ -8,20 +9,80 @@ void memInfo(char* buffer, size_t size) {
 		return;
 	}
 
-	if (fgets(buffer, size, file) != NULL) {
-		char* version_start = strstr(buffer, "MemTotal:");
-		if (version_start != NULL) {
-			version_start += strlen("MemTotal:") + 7;
+	int sizeFree = 0;
+	int sizeTotal = 0;
+	int sizeBuffers = 0;
+	int sizeCached = 0;
+	
+	while (fgets(buffer, size, file) != NULL) {
+		char* mem_total = strstr(buffer, "MemTotal:");
+		if (mem_total != NULL) {
+			mem_total += strlen("MemTotal:");
 
-			char* version_end = strchr(version_start, ' ');
-			if (version_end != NULL) {
-				*version_end = '\0';
+			while (*mem_total == ' ') {
+				mem_total++;
 			}
 
-			printf("Memory: %s kB\n", version_start);
+			char* mem_total_end = strchr(mem_total, ' ');
+			if (mem_total_end != NULL) {
+				*mem_total_end = '\0';
+			}
+			sscanf(mem_total, "%d", &sizeTotal);
+			sizeTotal /= 1024;
+		}
+
+		char* mem_free = strstr(buffer, "MemFree:");
+		if (mem_free != NULL) {
+			mem_free += strlen("MemFree:") + 1;
+
+			while (*mem_free == ' ') {
+				mem_free++;
+			}
+
+			char* mem_free_end = strchr(mem_free, ' ');
+			if (mem_free_end != NULL) {
+				*mem_free_end = '\0';
+			}
+			sscanf(mem_free, "%d", &sizeFree);	
+			sizeFree /= 1024;
+		}
+
+		char* buffers = strstr(buffer, "Buffers:");
+		if (buffers != NULL) {
+			buffers += strlen("Buffers:") + 1;
+
+			while (*buffers == ' ') {
+				buffers++;
+			}
+
+			char* buffers_end = strchr(buffers, ' ');
+			if (buffers_end != NULL) {
+				*buffers_end = '\0';
+			}
+			sscanf(buffers, "%d", &sizeBuffers);
+			sizeBuffers /= 1024;
+		}
+
+		char* cached = strstr(buffer, "Cached:");
+		if (cached != NULL && (cached == buffer || *(cached - 1) == '\n')) {
+			if (strncmp(cached, "SwapCached:", strlen("SwapCached:")) != 0) {
+				cached += strlen("Cached:");
+				
+				while (*cached == ' ') {
+					cached++;
+				}
+				
+				char* endptr;
+				long int tempCached = strtol(cached, &endptr, 10);
+
+				if (cached != endptr) {
+					sizeCached = (int) (tempCached / 1024);
+				}
+			}
 		}
 	}
-
+	int total = sizeTotal - sizeCached - sizeBuffers - sizeFree;
+	printf("Memory: %dMiB / %dMiB\n", total, sizeTotal);
 	fclose(file);
 }
 
